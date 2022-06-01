6c8cafa85ce9deb76cc81b18a65b2edb064560a7
Drew <DGriffin@appacademy.io>
2022-06-01 13:35:30 -0500

    Init get details route

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
-const fs = require('fs')
-const senators = require('../../senators.json')
+const fs = require('fs');
+const jsonQuery = require('json-query');
+const senators = require('../../senators.json');
 const router = express.Router();
 
 router.get('/senators', asyncHandler(async (req, res) => {
@@ -10,9 +11,21 @@ router.get('/senators', asyncHandler(async (req, res) => {
          const fullName = senator['person']['firstname'] + ' ' + senator['person']['lastname'];
         usSenators.push(fullName)
     })
-    console.log(senators)
-    res.send({ "usSenators": [usSenators] });
+
+    res.json({ "usSenators": usSenators });
 }));
+
+router.get('/senators/:name', asyncHandler(async (req, res) => {
+    const name = req.params.name;
+    const senatorDetails = {};
+    senators['objects'].forEach(senator => {
+        if (senator['person']['firstname'].toLocaleLowerCase() + senator['person']['lastname'] === name) {
+            senatorDetails["state"] = senator["state"];
+        }
+    })
+    res.send(senatorDetails);
+}));
+
 
 //tests router
 router.post('/test', function (req, res) {
